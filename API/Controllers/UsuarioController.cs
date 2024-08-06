53234c884aa7a5fa9294c9ef04bc21214098fcbc
eduardo 34 - pc Efrain <eduardomelchor34@gmail.com>
2024-08-05 21:25:31 -0600

    Hacer nuestro codigo asincrono

--- API/Controllers/UsuarioController.cs
+++ API/Controllers/UsuarioController.cs
@@ -1,6 +1,7 @@
 ﻿using Data;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Models.Entidades;
 
 namespace API.Controllers
@@ -17,16 +18,16 @@ namespace API.Controllers
         }
 
         [HttpGet] // api/usuario y retorna una lista de usuarios
-        public ActionResult<IEnumerable<Usuario>> GetUsuarios()
+        public async Task<ActionResult<IEnumerable<Usuario>>> GetUsuarios()
         {
-            var usuarios = _db.Usuarios.ToList();
+            var usuarios = await _db.Usuarios.ToListAsync();
             return Ok(usuarios);
         }
 
         [HttpGet("{id}")] // api/usuario/1
-        public ActionResult<Usuario> GetUsuario(int id)
+        public async Task<ActionResult<Usuario>> GetUsuario(int id)
         {
-            var usuario = _db.Usuarios.Find(id);
+            var usuario = await _db.Usuarios.FindAsync(id);
             return Ok(usuario);
         }
     }
