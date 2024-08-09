2045345ec28454288e6b94e0305fb25c2348a55b
eduardo 34 - pc Efrain <eduardomelchor34@gmail.com>
2024-08-08 19:23:05 -0600

    Authorize

--- API/Controllers/UsuarioController.cs
+++ API/Controllers/UsuarioController.cs
@@ -1,5 +1,6 @@
 ﻿using Data;
 using Data.Interfaces;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
@@ -24,6 +25,7 @@ namespace API.Controllers
 
         }
 
+        [Authorize]
         [HttpGet] // api/usuario y retorna una lista de usuarios
         public async Task<ActionResult<IEnumerable<Usuario>>> GetUsuarios()
         {
@@ -31,6 +33,7 @@ namespace API.Controllers
             return Ok(usuarios);
         }
 
+        [Authorize]
         [HttpGet("{id}")] // api/usuario/1
         public async Task<ActionResult<Usuario>> GetUsuario(int id)
         {
