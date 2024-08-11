91ea4ad9ac2af0ff796b141e6d6f93243b91b1c5
eduardom-34 <eduardomelchor34@gmail.com>
2024-08-11 15:28:22 -0600

    Especialidad Repositorio

---
+++ Data/Interfaces/IRepositorio/IRepositorioGenerico.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Data.Interfaces.IRepositorio
+{
+    internal interface IRepositorioGenerico<T> where T : class
+    {
+        Task<IEnumerable<T>> ObtenerTodos(
+            Expression<Func<T, bool>> Filtro = null,
+            Func<IQueryable<T>, IOrderedQueryable<T>> orderby = null,
+            string incluirPropiedades = null // Include
+            );
+
+        Task<T> ObtenerPrimero(
+            Expression<Func<T, bool>> Filtro = null,
+            string incluirPropiedades = null // Include
+            );
+
+        Task Agregar(T entidad);
+        void Remove(T entidad);
+    }
+}
