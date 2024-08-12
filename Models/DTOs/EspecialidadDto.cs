77f7ac54eaca6e9cd7dc7f1dcba791aaf2c10585
eduardom-34 <eduardomelchor34@gmail.com>
2024-08-11 20:26:41 -0600

    Especialidad controlaor

--- Models/DTOs/EspecialidadDto.cs
+++ Models/DTOs/EspecialidadDto.cs
@@ -11,14 +11,15 @@ namespace Models.DTOs
     {
         public int Id { get; set; }
 
-        [Required]
+        [Required(ErrorMessage = "El Nombre es Requerido")]
         [StringLength(60, MinimumLength = 1, ErrorMessage = "El nombre debe ser minimo 1 y max 60 caracteres")]
         public string NombreEspecialidad { get; set; }
 
-        [Required]
+        [Required(ErrorMessage = "La descripcion es Requerido")]
         [StringLength(100, MinimumLength = 1, ErrorMessage = "El nombre debe ser minimo 1 y max 100 caracteres")]
         public string Descripcion { get; set; }
 
+        [Required(ErrorMessage = "La estado es requerido")]
         public int Estado { get; set; } //1 - 0
     }
 }
